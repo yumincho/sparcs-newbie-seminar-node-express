dc26c5ddc8951ce8942203042c471b6e56e601c4
Yumin Cho <dbals081216@gmail.com>
2023-10-09 00:28:16 +0900

    Feat: impl example 2

--- client/src/pages/account.tsx
+++ client/src/pages/account.tsx
@@ -1,62 +1,120 @@
 import React from "react";
 import Header from "../components/header";
 import axios from "axios";
-import {SAPIBase} from "../tools/api";
+import { SAPIBase } from "../tools/api";
 import "./css/account.css";
 
 const AccountPage = () => {
-  const [ SAPIKEY, setSAPIKEY ] = React.useState<string>("");
-  const [ NBalance, setNBalance ] = React.useState<number | "Not Authorized">("Not Authorized");
-  const [ NTransaction, setNTransaction ] = React.useState<number | ''>(0);
+  // const [ SAPIKEY, setSAPIKEY ] = React.useState<string>("");
+  const [userId, setUserId] = React.useState<string>("");
+  const [userPw, setUserPw] = React.useState<string>("");
+  const [NBalance, setNBalance] = React.useState<number | "Not Authorized">(
+    "Not Authorized"
+  );
+  const [NTransaction, setNTransaction] = React.useState<number | "">(0);
 
   const getAccountInformation = () => {
-    const asyncFun = async() => {
-      interface IAPIResponse { balance: number };
-      const { data } = await axios.post<IAPIResponse>(SAPIBase + '/account/getInfo', { credential: SAPIKEY });
+    const asyncFun = async () => {
+      interface IAPIResponse {
+        balance: number;
+      }
+      const { data } = await axios.post<IAPIResponse>(
+        SAPIBase + "/account/getInfo",
+        { userId: userId, userPw: userPw }
+      );
       setNBalance(data.balance);
-    }
+    };
     asyncFun().catch((e) => window.alert(`AN ERROR OCCURED: ${e}`));
-  }
+  };
 
-  const performTransaction = ( amount: number | '' ) => {
-    const asyncFun = async() => {
-      if (amount === '') return;
-      interface IAPIResponse { success: boolean, balance: number, msg: string };
-      const { data } = await axios.post<IAPIResponse>(SAPIBase + '/account/transaction', { credential: SAPIKEY, amount: amount });
+  const performTransaction = (amount: number | "") => {
+    const asyncFun = async () => {
+      if (amount === "") return;
+      interface IAPIResponse {
+        success: boolean;
+        balance: number;
+        msg: string;
+      }
+      const { data } = await axios.post<IAPIResponse>(
+        SAPIBase + "/account/transaction",
+        { userId: userId, userPw: userPw, amount: amount }
+      );
       setNTransaction(0);
       if (!data.success) {
-        window.alert('Transaction Failed:' + data.msg);
+        window.alert("Transaction Failed:" + data.msg);
         return;
       }
-      window.alert(`Transaction Success! ₩${ NBalance } -> ₩${ data.balance }\nThank you for using SPARCS Bank`);
+      window.alert(
+        `Transaction Success! ₩${NBalance} -> ₩${data.balance}\nThank you for using SPARCS Bank`
+      );
       setNTransaction(0);
       setNBalance(data.balance);
-    }
+    };
     asyncFun().catch((e) => window.alert(`AN ERROR OCCURED: ${e}`));
-  }
+  };
 
   return (
     <div className={"account"}>
-      <Header/>
+      <Header />
       <h2>Account</h2>
       <div className={"account-token-input"}>
-        Enter API Key: <input type={"text"} value={SAPIKEY} onChange={e => setSAPIKEY(e.target.value)}/>
-        <button onClick={e => getAccountInformation()}>GET</button>
+        Id:
+        <input
+          type={"text"}
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+        />
+        PW:
+        <input
+          type="password"
+          value={userPw}
+          onChange={(e) => setUserPw(e.target.value)}
+        />
+        <button onClick={(e) => getAccountInformation()}>GET</button>
       </div>
       <div className={"account-bank"}>
         <h3>The National Bank of SPARCS API</h3>
         <div className={"balance"}>
           <p className={"balance-title"}>Current Balance</p>
-          <p className={"balance-value " + (typeof(NBalance) === "number" ? (NBalance >= 0 ? "balance-positive" : "balance-negative") : "")}>₩ { NBalance }</p>
+          <p
+            className={
+              "balance-value " +
+              (typeof NBalance === "number"
+                ? NBalance >= 0
+                  ? "balance-positive"
+                  : "balance-negative"
+                : "")
+            }
+          >
+            ₩ {NBalance}
+          </p>
         </div>
         <div className={"transaction"}>
-          ₩ <input type={"number"} value={NTransaction} min={0} onChange={e => setNTransaction(e.target.value === '' ? '' : parseInt(e.target.value))}/>
-          <button onClick={e => performTransaction(NTransaction)}>DEPOSIT</button>
-          <button onClick={e => performTransaction(NTransaction === '' ? '' : NTransaction * -1)}>WITHDRAW</button>
+          ₩{" "}
+          <input
+            type={"number"}
+            value={NTransaction}
+            min={0}
+            onChange={(e) =>
+              setNTransaction(
+                e.target.value === "" ? "" : parseInt(e.target.value)
+              )
+            }
+          />
+          <button onClick={(e) => performTransaction(NTransaction)}>
+            DEPOSIT
+          </button>
+          <button
+            onClick={(e) =>
+              performTransaction(NTransaction === "" ? "" : NTransaction * -1)
+            }
+          >
+            WITHDRAW
+          </button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default AccountPage;
--- seminar/src/middleware/auth.js
+++ seminar/src/middleware/auth.js
@@ -1,12 +1,14 @@
 const authMiddleware = (req, res, next) => {
-    if (req.body.credential === process.env.API_KEY) {
-        console.log("[AUTH-MIDDLEWARE] Authorized User");
-        next();
-    }
-    else {
-        console.log("[AUTH-MIDDLEWARE] Not Authorized User");
-        res.status(401).json({ error: "Not Authorized" });
-    }
-}
+  if (
+    req.body.userId === process.env.USER_ID &&
+    req.body.userPw === process.env.USER_PW
+  ) {
+    console.log("[AUTH-MIDDLEWARE] Authorized User");
+    next();
+  } else {
+    console.log("[AUTH-MIDDLEWARE] Not Authorized User");
+    res.status(401).json({ error: "Not Authorized" });
+  }
+};
 
 module.exports = authMiddleware;
